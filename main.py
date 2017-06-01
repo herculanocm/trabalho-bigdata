3c453d44b7aff46df54d4ae998f47f847bc6aded
Herculano da Cunha Madureira <herculanocm@acs.com.br>
2017-06-01 09:39:40 -0300

    Atualizando alteração do Humberto

--- main.py
+++ main.py
@@ -35,14 +35,42 @@ con =  bancoSqlite.connect()
 def retorna_lista_vendedores(df):
     return list(set(df['id_vendedor']))
 
+def apurar_vendas(df, id_vendedor):
+    qtd_vendas, qtd_produto = 0, 0
+    dfVendas = df.loc[df.id_vendedor == id_vendedor, ['num_nf','qtd_vendida']].groupby('num_nf').sum()
+    qtd_vendas = len(dfVendas)
+    qtd_produto = dfVendas['qtd_vendida'].sum()
+    return [id_vendedor, qtd_vendas, qtd_produto]
+
+def vendas_x_vendedor(df):
+    return df.loc[:, ['id_vendedor','num_nf']].groupby('id_vendedor').num_nf.nunique().reset_index().sort_values(by='num_nf', ascending=False)
+
+def produtos_x_vendedor(df):
+    return df.loc[:, ['id_vendedor', 'qtd_vendida']].groupby('id_vendedor').sum().sort_values(by='qtd_vendida', ascending=False)
 
 while True:
-    op = int(input("Digite uma das opções abaixo do Menu : \n 1) Lista de Vendedores Disponiveis. \n Digite 0 para sair. \n "))
+    strMenu = """ 
+    Digite uma das opções abaixo do Menu: 
+    1) Lista de Vendedores Disponíveis.
+    2) Selecionar um Vendedor.
+    3) Quantidade de Vendas x Vendedor.
+    4) Quantidade de Produtos x Vendedor.
+             
+    Digite 0 para sair.
+    """
+    op = int(input(strMenu))
     df = pd.read_csv('./base/pedidos.csv')
     if op == 0:
         break
     elif op == 1:
         print(retorna_lista_vendedores(df))
-
+    elif op == 2:
+        id_vendedor = input("Entre com o código do vendedor: ")
+        lstVendas = apurar_vendas(df, id_vendedor)
+        print(f'O vendedor {lstVendas[0]} totalizou {lstVendas[2]} produtos em {lstVendas[1]} vendas realizadas.')
+    elif op == 3:
+        print(vendas_x_vendedor(df))
+    elif op == 4:
+        print(produtos_x_vendedor(df))
 
 #TESTE DE COMMIT
